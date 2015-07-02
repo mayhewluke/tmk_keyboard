9cfbc3abd567e92213784d35e7f39e4f921232a5
Luke Mayhew <ldm@ldmayhew.ca>
2015-07-02 08:33:24 -0600

    Add Layer 2 - HJKL as arrow keys.

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -198,6 +198,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                 | TRNS | FN10 |------|       |------|  0   | TRNS |
      *                                 |      |      | FN11 |       | FN12 |      |      |
      *                                 `--------------------'       `--------------------'
+     *
+     * Layer 2:
+     * ,--------------------------------------------------.           ,--------------------------------------------------.
+     * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |           | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |  TRNS  |
+     * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+     * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |           | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |  TRNS  |
+     * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+     * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS |------|           |------| LEFT | DOWN |  UP  | RIGHT| TRNS |  TRNS  |
+     * |--------+------+------+------+------+------| TRNS |           | TRNS |------+------+------+------+------+--------|
+     * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS |      |           |      | TRNS | TRNS | TRNS | TRNS | TRNS |  TRNS  |
+     * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+     *   | TRNS | TRNS | TRNS | TRNS | TRNS |                                       | TRNS | TRNS | TRNS | TRNS | TRNS |
+     *   `----------------------------------'                                       `----------------------------------'
+     *                                        ,-------------.       ,-------------.
+     *                                        |      | TRNS |       |      |      |
+     *                                 ,------|------|------|       |------+------+------.
+     *                                 |      |      |      |       |      |      |      |
+     *                                 | TRNS | TRNS |------|       |------| TRNS | TRNS |
+     *                                 |      |      | TRNS |       | TRNS |      |      |
+     *                                 `--------------------'       `--------------------'
      */
 
     KEYMAP(  // Layer0, Left hand.
@@ -244,5 +264,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_NO, KC_TRNS,
          KC_NO,
        KC_FN12,    KC_0, KC_TRNS
+    ),
+
+    KEYMAP(  // Layer2, left hand
+        KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,        KC_TRNS,     KC_TRNS, KC_TRNS,
+        KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,        KC_TRNS,     KC_TRNS, KC_TRNS,
+        KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,        KC_TRNS,     KC_TRNS,
+        KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,        KC_TRNS,     KC_TRNS, KC_TRNS,
+        KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,        KC_TRNS,
+
+                                          KC_TRNS,  KC_TRNS,
+                                                    KC_TRNS,
+                                KC_TRNS,  KC_TRNS,  KC_TRNS,
+
+        // right hand
+             KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS,
+             KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS,
+                               KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,     KC_TRNS, KC_TRNS,
+             KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS,
+                                        KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS,
+
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
