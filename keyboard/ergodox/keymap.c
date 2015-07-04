7ea3233cfde418c55e7e2b25664d8f23359d68aa
Luke Mayhew <ldm@ldmayhew.ca>
2015-07-04 09:39:37 -0600

    Add decimal character to the numpad on Layer 2

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -189,7 +189,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+------| TRNS |           | TRNS |------+------+------+------+------+--------|
      * |  FN9   |  *   |  @   |  {   |  }   |  &   |      |           |      |  :   |  1   |  2   |  3   | FN13 |  FN14  |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   | TRNS | TRNS | TRNS | TRNS | TRNS |                                       |  0   | TRNS | TRNS | TRNS | TRNS |
+     *   | TRNS | TRNS | TRNS | TRNS | TRNS |                                       |  0   | TRNS |  .   | TRNS | TRNS |
      *   `----------------------------------'                                       `----------------------------------'
      *                                        ,-------------.       ,-------------.
      *                                        |      |Teensy|       |      |      |
@@ -259,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS,   SHIFT(KC_BSLS),    KC_7,    KC_8,    KC_9, SHIFT(KC_6), KC_TRNS,
                                KC_BSLS,    KC_4,    KC_5,    KC_6,   KC_SCOLON, KC_TRNS,
              KC_TRNS, SHIFT(KC_SCOLON),    KC_1,    KC_2,    KC_3,     KC_FN13, KC_FN14,
-                                           KC_0, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
+                                           KC_0, KC_TRNS,  KC_DOT,     KC_TRNS, KC_TRNS,
 
          KC_NO, KC_TRNS,
          KC_NO,
