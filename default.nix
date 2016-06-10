6a786282b670555aaa7f97badcc50c1b43c99574
Luke Mayhew <ldm@ldmayhew.ca>
2016-06-10 11:46:54 -0600

    Nixify

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, keyboard ? "ergodox" }:
+
+with nixpkgs;
+with pkgs;
+
+let
+  loader = "${teensy-loader-cli}/bin/teensy-loader-cli";
+  subdir = "keyboard/${keyboard}";
+  make   = "make $makeFlags -f $makefile --no-print-directory";
+  script = "make-teensy";
+in
+  stdenv.mkDerivation rec {
+    name = "tmk-keyboard-${keyboard}";
+    src = ./.;
+    buildInputs = [ avrgcclibc ];
+    makefile = "Makefile.lufa";
+    makeFlags = "-C ${subdir}";
+    installPhase = ''
+      mkdir -p $out
+
+      # Copy build results
+      ${make} program PROGRAM_CMD="cp -p \$^ $out/"
+
+      # Put teensy target commands into a script
+      cat >$out/${script} <<EOF
+      #!/bin/sh
+      cd $out
+      exec $(${make} -n teensy | sed 's@teensy_loader_cli@${loader}@g')
+      EOF
+      chmod a+x $out/${script}
+    '';
+  }
